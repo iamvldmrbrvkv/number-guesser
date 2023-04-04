f5f86f7f4bd3d4f00b2ef00d70e2a1ee83b7b8f0
Владимир Боровиков <vladimirborovikov@MacBook-Air-Vladimir.local>
2023-04-04 11:31:58 +0300

    finished app

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
 
+const compareGuesses = (userGuess, computerGuess, secretNumber) => {
+    const userDifferense = Math.abs(userGuess - secretNumber);
+    const computerDifferense = Math.abs(computerGuess - secretNumber);
+    if (userDifferense >= computerDifferense) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        return humanScore++;
+    } else {
+        return computerScore++;
+    }
+}
+
+const advanceRound = () => currentRoundNumber++;
